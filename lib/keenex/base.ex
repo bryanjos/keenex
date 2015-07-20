7927dd493b0bda11b8fb8bbc4b512ae509e24c9d
Gullit Miranda <gullitmiranda@gmail.com>
2015-07-20 15:14:16 -0300

    Adding back the `to_response` method in `Keenex.Base`

--- lib/keenex/base.ex
+++ lib/keenex/base.ex
@@ -19,28 +19,38 @@ defmodule Keenex.Base do
   def get(endpoint) do
     url(endpoint)
     |> Http.get
+    |> to_response
   end
 
   def post(endpoint, data) do
     url(endpoint)
     |> Http.post(Poison.encode!(data))
+    |> to_response
   end
 
   def post(endpoint) do
     post(endpoint, "")
+    |> to_response
   end
 
   def put(endpoint, data) do
     url(endpoint)
     |> Http.put(Poison.encode!(data))
+    |> to_response
   end
 
   def put(endpoint) do
     put(endpoint, "")
+    |> to_response
   end
 
   def delete(endpoint) do
     url(endpoint)
     |> Http.delete
+    |> to_response
+  end
+
+  def to_response({status, response}) do
+    {status, Poison.decode!(response)}
   end
 end
